697b3eb1fb263dc44e32a5d573176251d72f99da
azu <azuciao@gmail.com>
2013-12-01 20:14:58 +0900

    breaking change : default execute pattern

--- tests/power-doctest.test.js
+++ tests/power-doctest.test.js
@@ -8,7 +8,7 @@ describe("power-doctest", function () {
     describe("#convertFromCodeToTree", function () {
         it("When expression + comment", function () {
             var code = "var a = 1;\n" +
-                "a;// > 1";
+                "a;// => 1";
             var resultAST = docPower.convertFromCodeToTree(code);
             assertAST(resultAST, function () {
                 var a = 1;
@@ -16,15 +16,15 @@ describe("power-doctest", function () {
             });
         });
         it("simple case", function () {
-            var code = "1; // > 1"
+            var code = "1; // => 1"
             var resultAST = docPower.convertFromCodeToTree(code);
             assertAST(resultAST, function () {
                 assert(1 === 1);
             });
         });
         it("when test x 2", function () {
-            var code = "1; // > 1\n" +
-                "2; // > 2\n"
+            var code = "1; // => 1\n" +
+                "2; // => 2\n"
             var resultAST = docPower.convertFromCodeToTree(code);
             assertAST(resultAST, function () {
                 assert(1 === 1);
@@ -33,7 +33,7 @@ describe("power-doctest", function () {
         });
         it("when equal to object", function () {
             var code = "var a = {a : 1};\n" +
-                "a; // > {a : 1}"
+                "a; // => {a : 1}"
             var resultAST = docPower.convertFromCodeToTree(code);
             assertAST(resultAST, function () {
                 var a = {a: 1};
@@ -42,7 +42,7 @@ describe("power-doctest", function () {
         });
         it("When CallExpression", function () {
             var code = "var a = function(){return 1;};" +
-                "a(); // > 1";
+                "a(); // => 1";
             var resultAST = docPower.convertFromCodeToTree(code);
             assertAST(resultAST, function () {
                 var a = function () {
@@ -53,7 +53,7 @@ describe("power-doctest", function () {
         });
         it("When BinaryExpression", function () {
             var code = "var a = function(){return 1;};\n" +
-                "a + 1; // > 2";
+                "a + 1; // => 2";
             var resultAST = docPower.convertFromCodeToTree(code);
             assertAST(resultAST, function () {
                 var a = function () {
@@ -63,7 +63,7 @@ describe("power-doctest", function () {
             });
         });
         it("When BlockComment", function () {
-            var code = "1; /* > 1 */";
+            var code = "1; /* => 1 */";
             var resultAST = docPower.convertFromCodeToTree(code);
             assertAST(resultAST, function () {
                 assert(1 === 1);
@@ -73,15 +73,15 @@ describe("power-doctest", function () {
     });
     describe("#regexpExecuted", function () {
         beforeEach(function () {
-            docPower.regexpExecuted(/\s+==>\s(.*?)$/);
+            docPower.regexpExecuted(/\s+===\s(.*?)$/);
         });
         afterEach(function () {
             // reset
-            docPower.regexpExecuted(/\s+>\s+(.*)$/);
+            docPower.regexpExecuted(/\s+=>\s+(.*)$/);
         });
         it("accept regexp comment ", function () {
             var code = "var a = 1;" +
-                "a; // ==> 1";
+                "a; // === 1";
             var resultAST = docPower.convertFromCodeToTree(code);
             assertAST(resultAST, function () {
                 var a = 1;
@@ -93,7 +93,7 @@ describe("power-doctest", function () {
         context("when success test", function () {
             it("should not result message", function () {
                 var code = "var a = 1;\n" +
-                    "a; // > 1";
+                    "a; // => 1";
                 var resultMessage = docPower.runDocTest(code);
                 assert.isUndefined(resultMessage);
             });
@@ -101,7 +101,7 @@ describe("power-doctest", function () {
         context("when fail test", function () {
             it("should output message", function () {
                 var code = "var a = 'test';\n" +
-                    "a; // > 'not match'\n";
+                    "a; // => 'not match'\n";
                 var resultMessage = docPower.runDocTest(code);
                 assert.isNotNull(resultMessage);
             });
