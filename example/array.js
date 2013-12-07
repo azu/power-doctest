ca8435b8bfdf1ed150875061f51ab9904eec816e
azu <azuciao@gmail.com>
2013-12-07 20:14:19 +0900

    add inline options

--- example/array.js
+++ example/array.js
@@ -1,2 +1,11 @@
-var ary = [1, 2, 3];
-ary; // => ["it", "is not" ,"match"]
+var a = [1, 2, 3];
+a; // => [1 ,2,3]
+
+
+function sum(ary) {
+    return ary.reduce(function (current, next) {
+        return current + next
+    }, 0);
+}
+
+sum([1, 2, 3, 4, 5]); // > 5
