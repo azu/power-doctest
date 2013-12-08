3a3a979e764e7ae2bf3eec1048cadcb48fbe9eda
azu <azuciao@gmail.com>
2013-12-08 20:26:41 +0900

    filter message of power-assert.

--- example/array.js
+++ example/array.js
@@ -1,11 +1,2 @@
-var a = [1, 2, 3];
-a; // => [1 ,2,3]
-
-
-function sum(ary) {
-    return ary.reduce(function (current, next) {
-        return current + next
-    }, 0);
-}
-
-sum([1, 2, 3, 4, 5]); // => 5
+var ary = ["test","is","array"];
+ary; // => ["test", "was", "array"]
