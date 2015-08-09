d9e1a7c6a58c933e0a399d7b4edbcbaba216c175
azu <azuciao@gmail.com>
2015-08-09 13:55:00 +0900

    feat(cli): add cli for power-doctest

--- src/power-doctest.js
+++ src/power-doctest.js
@@ -4,6 +4,15 @@ import {toAssertFromAST} from "comment-to-assert"
 import {injectAssertModule} from "./inject-assert"
 import empower from "empower"
 import assert from "assert"
+import ASTSource from "ast-source"
+export function convertCode(code, filePath) {
+    var source = new ASTSource(code, {
+        filePath: filePath,
+        disableSourceMap: typeof filePath === "undefined"
+    });
+    var output = source.transform(convertAST).output();
+    return output.codeWithMap;
+}
 
 export function convertAST(AST) {
     var empowerOptions = {modifyMessageOnRethrow: true, saveContextOnRethrow: true};
