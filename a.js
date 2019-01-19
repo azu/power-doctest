0878c2675bec6a1d532d50370e7e6497e2764ae6
azu <azuciao@gmail.com>
2019-01-19 21:40:51 +0900

    chore: remove

--- a.js
+++
@@ -1,4 +0,0 @@
-var assert = require("power-assert");
-assert.strictEqual("文字列", "文字列"); // => "文字列"
-assert.strictEqual("\u6587\u5b57\u5217", "文字列"); // => "文字列"
-assert.strictEqual("𩸽"[0], "\uD867"); // => "\uD867"
