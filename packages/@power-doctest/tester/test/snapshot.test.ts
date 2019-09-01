f7a6ff20e3ddd2c92429db756e119ee1698f54b9
azu <azuciao@gmail.com>
2019-09-01 13:29:45 +0900

    test(@power-doctest/tester): add snapshots

--- packages/@power-doctest/tester/test/snapshot.test.ts
+++ packages/@power-doctest/tester/test/snapshot.test.ts
@@ -1,8 +1,13 @@
 import * as fs from "fs";
 import * as path from "path";
 import * as assert from "assert";
-import { run } from "../src";
+import { run, test } from "../src";
+import { parse } from "@power-doctest/javascript";
 
+const allSettled = require("promise.allsettled");
+const trimUndefinedProperty = <T>(o: T): T => {
+    return JSON.parse(JSON.stringify(o));
+};
 const normalizeErrorName = (error: string) => {
     const match = error.match(/(.*Error)/);
     return match && match[1];
@@ -12,7 +17,7 @@ describe("Snapshot testing", () => {
     fs.readdirSync(fixturesDir)
         .map(caseName => {
             const normalizedTestName = caseName.replace(/-/g, " ");
-            it(`Test ${normalizedTestName}`, async function() {
+            it(`Run ${normalizedTestName}`, async function() {
                 const fixtureDir = path.join(fixturesDir, caseName);
                 const actualFilePath = path.join(fixtureDir, "input.js");
                 const actualContent = fs.readFileSync(actualFilePath, "utf-8");
@@ -40,6 +45,42 @@ describe("Snapshot testing", () => {
 ${fixtureDir}
 ${actual}
 `
+                );
+            });
+            it(`Test ${normalizedTestName}`, async function() {
+                const fixtureDir = path.join(fixturesDir, caseName);
+                const actualFilePath = path.join(fixtureDir, "input.js");
+                const actualContent = fs.readFileSync(actualFilePath, "utf-8");
+                const parsedResults = parse({
+                    content: actualContent,
+                    filePath: actualFilePath
+                });
+                const promises = parsedResults.map(result => {
+                    return test(result);
+                });
+                const actual = await allSettled(promises);
+                const results = trimUndefinedProperty(actual.map((result: any) => {
+                    if (result.status === "rejected") {
+                        return {
+                            status: result.status,
+                            message: normalizeErrorName(result.reason.name)
+                        };
+                    }
+                    return result;
+                }));
+                const expectedFilePath = path.join(fixtureDir, "output.json");
+                // Usage: update snapshots
+                // UPDATE_SNAPSHOT=1 npm test
+                if (!fs.existsSync(expectedFilePath) || process.env.UPDATE_SNAPSHOT) {
+                    fs.writeFileSync(expectedFilePath, JSON.stringify(results, null, 4));
+                    this.skip(); // skip when updating snapshots
+                    return;
+                }
+                // compare input and output
+                const expectedContent = JSON.parse(fs.readFileSync(expectedFilePath, "utf-8"));
+                assert.deepStrictEqual(
+                    results,
+                    expectedContent
                 );
             });
         });
