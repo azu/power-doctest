b368ab32fb876e2acaad01a0d8e6834d9b8ab5d6
azu <azuciao@gmail.com>
2019-09-01 14:38:00 +0900

    test:(types): add test

---
+++ packages/@power-doctest/types/test/index.test.ts
@@ -0,0 +1,25 @@
+import { ParsedResults, ParsedCode, ParserArgs } from "../src";
+
+it("is valid types", () => {
+    const parse = (args: ParserArgs): ParsedResults => {
+        const code: ParsedCode = {
+            code: args.content,
+            state: "none",
+            location: {
+                start: {
+                    line: 1,
+                    column: 0
+                },
+                end: {
+                    line: 1,
+                    column: 0
+                }
+            }
+        };
+        return [code];
+    };
+    parse({
+        content: "test",
+        filePath: "test.md"
+    });
+});
